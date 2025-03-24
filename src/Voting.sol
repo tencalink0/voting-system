47843133b8742bafcb53a9288dc7bb3e8423affe
tencalink0 <bencax270@gmail.com>
2025-03-24 20:46:23 +0000

    voting: complete contract (not tested)

--- src/Voting.sol
+++ src/Voting.sol
@@ -2,12 +2,70 @@
 pragma solidity ^0.8.0;
 
 contract Voting {
+    struct Candidate {
+        string name;
+        address addr;
+    }
+
     mapping(address => bool) public voters;
-    mapping(string => uint256) public votes;
+    mapping(address => uint256) public votes;
+    mapping(string => address) public candidateAddr;
+    mapping(address => string) public candidateName;
 
-    string[] public candidates;
+    address[] public candidates;
+    uint256 public immutable votingDeadline;
 
-    constructor(string[] memory _candidates) {
-        candidates = _candidates;
+    event VotingEnded(address[] winners);
+
+    constructor(Candidate[] memory _candidates, uint256 _votePeriod) {
+        for (uint256 i = 0; i < _candidates.length; i++) {
+            candidates.push(_candidates[i].addr);
+            candidateAddr[_candidates[i].name] = _candidates[i].addr;
+        }
+        votingDeadline = block.timestamp + _votePeriod;
+    }
+
+    function vote(address _candidate) external {
+        require(block.timestamp < votingDeadline, 'Voting has ended');
+        require(bytes(candidateName[_candidate]).length > 0, 'Candidate does not exist');
+        require(voters[msg.sender] == true, 'Already voted');
+
+        votes[_candidate] += 1;
+    }
+
+    function getCandidateAddr(string calldata _name) external view returns (address) {
+        return(candidateAddr[_name]);
+    }
+
+    function findWinners() public view returns (address[] memory winners, uint256 maxVotes) {
+        maxVotes = 0;
+        address[] memory tempWinners = new address[](candidates.length);
+        uint256 winnerCount = 0;
+
+        for (uint256 i = 0; i < candidates.length; i++) {
+            uint256 currentVotes = votes[candidates[i]];
+            if (currentVotes > maxVotes && maxVotes > 0) {
+                maxVotes = currentVotes;
+                winnerCount = 0;
+                tempWinners[winnerCount] = candidates[i];
+                winnerCount++;
+            } else if (currentVotes == maxVotes) {
+                tempWinners[winnerCount] = candidates[i];
+                winnerCount++;
+            }
+        }
+
+        winners = new address[](winnerCount);
+        for (uint256 i = 0; i < winnerCount; i++) {
+            winners[i] = tempWinners[i];
+        }
+    }
+
+    function callWinner() external {
+        require(block.timestamp >= votingDeadline, "Voting hasn't ended yet");
+        (address[] memory winners, ) = findWinners();
+
+        // TODO: convert addr to names
+        emit VotingEnded(winners);
     }
 }
