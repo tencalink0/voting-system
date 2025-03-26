7507fa99e5693116aa6b1b1b901a7ff9a7c0fb5e
tencalink0 <bencax270@gmail.com>
2025-03-26 21:26:56 +0000

    target cleanup

--- target/debug/deps/voting_system-dbd49827f2f76475.d
+++
@@ -1,5 +0,0 @@
-C:\Users\benca\dev\voting-system\target\debug\deps\libvoting_system-dbd49827f2f76475.rmeta: src/main.rs
-
-C:\Users\benca\dev\voting-system\target\debug\deps\voting_system-dbd49827f2f76475.d: src/main.rs
-
-src/main.rs:
