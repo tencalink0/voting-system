b9cb735073fc976cc27bffc4fe5e952e47d65e0b
tencalink0 <bencax270@gmail.com>
2025-04-21 16:52:18 +0100

    feat: abi add

--- frontend/src/contract.ts
+++ frontend/src/contract.ts
@@ -1,5 +1,210 @@
 export const contractAddress = "0xYourContractAddress";
 
 export const contractABI = [
-  // Paste ABI here from `out/Voting.sol/Voting.json`
+    {
+        "type": "constructor",
+        "inputs": [
+            {
+                "name": "_candidates",
+                "type": "tuple[]",
+                "internalType": "struct Voting.Candidate[]",
+                "components": [
+                    {
+                        "name": "name",
+                        "type": "string",
+                        "internalType": "string"
+                    },
+                    {
+                        "name": "addr",
+                        "type": "address",
+                        "internalType": "address"
+                    }
+                ]
+            },
+            {
+                "name": "_votePeriod",
+                "type": "uint256",
+                "internalType": "uint256"
+            }
+        ],
+        "stateMutability": "nonpayable"
+    },
+    {
+        "type": "function",
+        "name": "callWinner",
+        "inputs": [],
+        "outputs": [],
+        "stateMutability": "nonpayable"
+    },
+    {
+        "type": "function",
+        "name": "candidateAddr",
+        "inputs": [
+            {
+                "name": "",
+                "type": "string",
+                "internalType": "string"
+            }
+        ],
+        "outputs": [
+            {
+                "name": "",
+                "type": "address",
+                "internalType": "address"
+            }
+        ],
+        "stateMutability": "view"
+    },
+    {
+        "type": "function",
+        "name": "candidateName",
+        "inputs": [
+            {
+                "name": "",
+                "type": "address",
+                "internalType": "address"
+            }
+        ],
+        "outputs": [
+            {
+                "name": "",
+                "type": "string",
+                "internalType": "string"
+            }
+        ],
+        "stateMutability": "view"
+    },
+    {
+        "type": "function",
+        "name": "candidates",
+        "inputs": [
+            {
+                "name": "",
+                "type": "uint256",
+                "internalType": "uint256"
+            }
+        ],
+        "outputs": [
+            {
+                "name": "",
+                "type": "address",
+                "internalType": "address"
+            }
+        ],
+        "stateMutability": "view"
+    },
+    {
+        "type": "function",
+        "name": "findWinners",
+        "inputs": [],
+        "outputs": [
+            {
+                "name": "winners",
+                "type": "address[]",
+                "internalType": "address[]"
+            },
+            {
+                "name": "maxVotes",
+                "type": "uint256",
+                "internalType": "uint256"
+            }
+        ],
+        "stateMutability": "view"
+    },
+    {
+        "type": "function",
+        "name": "getCandidateAddr",
+        "inputs": [
+            {
+                "name": "_name",
+                "type": "string",
+                "internalType": "string"
+            }
+        ],
+        "outputs": [
+            {
+                "name": "",
+                "type": "address",
+                "internalType": "address"
+            }
+        ],
+        "stateMutability": "view"
+    },
+    {
+        "type": "function",
+        "name": "vote",
+        "inputs": [
+            {
+                "name": "_candidate",
+                "type": "address",
+                "internalType": "address"
+            }
+        ],
+        "outputs": [],
+        "stateMutability": "nonpayable"
+    },
+    {
+        "type": "function",
+        "name": "voters",
+        "inputs": [
+            {
+                "name": "",
+                "type": "address",
+                "internalType": "address"
+            }
+        ],
+        "outputs": [
+            {
+                "name": "",
+                "type": "bool",
+                "internalType": "bool"
+            }
+        ],
+        "stateMutability": "view"
+    },
+    {
+        "type": "function",
+        "name": "votes",
+        "inputs": [
+            {
+                "name": "",
+                "type": "address",
+                "internalType": "address"
+            }
+        ],
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint256",
+                "internalType": "uint256"
+            }
+        ],
+        "stateMutability": "view"
+    },
+    {
+        "type": "function",
+        "name": "votingDeadline",
+        "inputs": [],
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint256",
+                "internalType": "uint256"
+            }
+        ],
+        "stateMutability": "view"
+    },
+    {
+        "type": "event",
+        "name": "VotingEnded",
+        "inputs": [
+            {
+                "name": "winners",
+                "type": "address[]",
+                "indexed": false,
+                "internalType": "address[]"
+            }
+        ],
+        "anonymous": false
+    }
 ];
--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -5,13 +5,12 @@ import "forge-std/Script.sol";
 import "../src/Voting.sol";
 
 contract Deploy is Script {
+    Voting.Candidate[] public candidates;
+    
     function run() external {
-        // Candidate setup
-        Voting.Candidate ;
         candidates[0] = Voting.Candidate({name: "tencalink0", addr: 0x1000000000000000000000000000000000000001});
         candidates[1] = Voting.Candidate({name: "bev29rr", addr: 0x2000000000000000000000000000000000000002});
 
-        // Broadcast deployment
         vm.startBroadcast();
         Voting voting = new Voting(candidates, 5 days);
         vm.stopBroadcast();
